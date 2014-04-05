f526077d8e028f87e0e8a1c0df7247f21c6a92d5
Sam-Hentschel <samuel_hentschel@fis.edu>
2014-04-05 04:21:51 +0200

    Create RFCDownloader and format update

--- RFCDownloader.sh
+++ RFCDownloader.sh
@@ -6,68 +6,61 @@
 #Function to display the usage of the command
 function usage()
 {
-    printf "    phrackdownloader -d downloads_file_path -f main_folder_name [-h] [-i] -m newest_issue_of_phrack -t target_file_path [-v]\n"
+    printf "    rfcdownloader -d downloads_file_path -f main_folder_name [-h] [-i] [-p] -t target_file_path [-v]\n"
     printf "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n"
     printf "    d downloads_file_path -/- sets the path to your downloads folder (required if -i, or -h is not activated)\n"
     printf "    f main_folder_name -/- sets the name of the main folder (if not set, default is \"Philes\")\n"
     printf "    h -/- prints the help usage page (aka, this one)\n"
     printf "    i -/- turns on interactive mode (creates a dialogue)\n"
-    printf "    m -/- tells the program which issue to download until (should be set to the newest)\n"
+    printf "    p -/- downloads the files in pdf format"
     printf "    t target_file_path -/- sets the path to your target folder (required if -i, or -h is not activated)\n"
-    printf '    v -/- sets verbose mode on\n'
+    printf "    v -/- sets verbose mode on\n"
 }
 
 #Function to take the input data and deliver the folders to the users
 function maker()
 {
     mkdir ${target}/${folder}
-    for i in $(seq $min $max)
-    do
-        name=$i
-        echo '...Downloading File...'
-        cd ${down}
-        curl -O http://www.phrack.org/archives/tgz/phrack${name}.tar.gz
-        echo '-----Download Complete-----'
-        downloads_g=${down}/phrack${name}.tar.gz
-        echo '...Ungunzipping file...'
-        gunzip $downloads_g
-        echo '-----Finished Ungunning the file-----'
-        downloads_t=${down}/phrack${name}.tar
-        echo '...Untarring file...'
-        tar -xzvf $downloads_t
-        echo '-----Finished Untarring the file-----'
-        download=${down}/${name}
-        echo '...Moving file...'
-        endpath=${target}/${folder}
-        mv $download $endpath
-        echo '-----Moved file-----'
-        echo '...Checking and fixing extension...'
-        for f in ${endpath}/${name}
-        do
-            if [ ${f: -4} != '.txt' ]
-            then
-                find $f -type f -not -name "*.*" -exec mv "{}" "{}".txt \;
-            fi
-        done
-        echo '-----Extensions correct-----'
-        echo '...Deleting original files...'
-        rm $downloads_t
-        echo '-----Original Files Deleted-----'
-    done
+    echo '...Downloading File...'
+    echo 'This may take a while.'
+    cd ${down}
+    curl -O ftp://ftp.rfc-editor.org/in-notes/tar/RFC-all.tar.gz
+    echo '-----Download Complete-----'
+    downloads_g=${down}/RFC-all.tar.gz
+    echo '...Ungunzipping file...'
+    gunzip $downloads_g
+    echo '-----Finished Ungunning the file-----'
+    downloads_t=${down}/RFC-all.tar
+    echo '...Untarring file...'
+    tar -xzvf $downloads_t
+    echo '-----Finished Untarring the file-----'
+    download=${down}/RFC-all
+    echo '...Moving file...'
+    endpath=${target}/${folder}
+    mv $download $endpath
+    echo '-----Moved file-----'
+    echo '...Deleting original files...'
+    rm $downloads_t
+    echo '-----Original Files Deleted-----'
 }
 
 #Function to initiate interactive mode of input
 function interactive()
 {
-    echo 'What is the newest issue of Phrack? '
-    read max
     echo 'What is the file path to your downloads folder?'
     read down
     echo 'What is the file path to your destination folder?'
     read target
     echo 'What would you like the folder name for the storage of the issues to be?'
     read folder
-    maker
+    echo 'Would you like it in pdf or txt format? (type the one you would like)'
+    read ans
+    if [ "$ans" == "pdf" ]
+    then
+        pdfmaker
+    else
+        maker
+    fi
 }
 
 #Starts to check the options and start the respective next function (interactive or maker)
@@ -88,8 +81,8 @@ do
             interact=1
             break
             ;;
-        m)
-            max=${OPTARG}
+        p)
+            pdf=1
             ;;
         t)
             target=${OPTARG}
@@ -106,6 +99,9 @@ done
 if [ "$interact" == "1" ]
 then
     interactive
+elif [ "$pdf" == "1" ]
+then
+    pdfmaker
 else
     maker
 fi
